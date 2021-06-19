97e483e0c1dd1bd05826e2f7371c995324d70111
Dorijan-Cirkveni <33494690+Dorijan-Cirkveni@users.noreply.github.com>
2021-06-19 23:40:54 +0200

    Some name changes
    following the little code I changed so far

--- src/amount.h
+++ src/amount.h
@@ -3,7 +3,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_AMOUNT_H
+#ifndef BITCOIN_AMOUNT_H //too lazy to change all the variable names now lol. just pretend this says SMOLCOIN
 #define BITCOIN_AMOUNT_H
 
 #include <stdint.h>
@@ -19,16 +19,16 @@ typedef int64_t CAmount;
 static const CAmount SATOSHIS = 100000000
 static const CAmount COIN = 100000000 * SATOSHIS;
 
-/** No amount larger than this (in satoshi) is valid.
+/** No amount larger than this (in nakamotos) is valid.
  *
  * Note that this constant is *not* the total money supply, which in Bitcoin
- * currently happens to be less than 21,000,000 BTC for various reasons, but
+ * currently happens to be less than 0.000 021 SMC for various reasons, but
  * rather a sanity check. As this sanity check is used by consensus-critical
  * validation code, the exact value of the MAX_MONEY constant is consensus
  * critical; in unusual circumstances like a(nother) overflow bug that allowed
  * for the creation of coins out of thin air modification could lead to a fork.
  * */
-static const CAmount MAX_MONEY = 2100 * SATOSHIS;
+static const CAmount MAX_MONEY = 2100 * SATOSHIS; // aka 0.000 021 SMC
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
 #endif //  BITCOIN_AMOUNT_H
